e7d36ab73b3b90f3921239d9eccea5da9b82ce0a
Grigory Moshkevich <ilcostuff@gmail.com>
2017-12-15 21:39:26 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
-var gulp = require('gulp');
+const gulp = require('gulp')
+  sass = require('gulp-sass')
+  concat = require('gul-concat');
+
+gulp.task('hey', function() {
+  console.log('hey');
+});
+
+gulp.task('sass', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(concat())
+    // .pipe(sass())
+    .pipe(gulp.dest(''))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('*.scss', ['sass']);
+})
