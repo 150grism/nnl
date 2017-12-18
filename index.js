80f4a966d3c24a2d5b79185ee2567c736f8a10b2
Grigory Moshkevich <ilcostuff@gmail.com>
2017-12-18 19:43:13 +0300

    .

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var headers = document.querySelectorAll('.accordion__header');
 var contents = document.querySelectorAll('.accordion__content');
-var bodies = document.querySelectorAll('.accordion__body');
-var footers = document.querySelectorAll('.accordion__footer');
 var chevrons = document.querySelectorAll('.accordion__chevron');
 
-console.log(contents[0].scrollHeight);
-console.log(footers[0].scrollHeight);
-
 for (let i = 0; i < contents.length; i++) {
-  console.log(footers[i].scrollHeight);
   headers[i].addEventListener('click', function() {
-    console.log(bodies[i].scrollHeight, footers[i].scrollHeight);
-    let contentHeight = bodies[i].scrollHeight + footers[i].scrollHeight; //contents[i].scrollHeight;
+    let contentHeight = contents[i].scrollHeight;
     contents[i].classList.toggle('accordion__content_visible');
     chevrons[i].classList.toggle('fa-rotate-90');
     if (contents[i].classList.contains('accordion__content_visible')) {
