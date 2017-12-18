a7d768f12d5c04dbe898c7127b91039264633e7b
Grigory Moshkevich <ilcostuff@gmail.com>
2017-12-18 22:13:29 +0300

    .

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var headers = document.querySelectorAll('.accordion__header');
 var contents = document.querySelectorAll('.accordion__content');
 var chevrons = document.querySelectorAll('.accordion__chevron');
 
+var menuList = document.querySelector('.menu__list');
+var menuItems = document.querySelectorAll('.menu__item');
+var o = [];
+
 for (let i = 0; i < contents.length; i++) {
   headers[i].addEventListener('click', function() {
     let contentHeight = contents[i].scrollHeight;
@@ -14,3 +18,14 @@ for (let i = 0; i < contents.length; i++) {
     }
   });
 }
+
+menuList.addEventListener('click', function() {
+  for (let i = 0; i < menuItems.length; i++) {
+    console.log(menuItems[i].children[0].children[0].checked);
+    if (menuItems[i].children[0].children[0].checked) {
+      menuItems[i].classList.add('menu__item_active');
+    } else {
+      menuItems[i].classList.remove('menu__item_active');
+    }
+  }
+});
