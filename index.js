3782a991753d6cbb1bfba4c6fe6d3496af554234
Grigory Moshkevich <ilcostuff@gmail.com>
2017-12-18 18:00:01 +0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var headers = document.querySelectorAll('.accordion__header');
 var contents = document.querySelectorAll('.accordion__content');
+var icons = document.querySelectorAll('.accordion__icon');
 
 for (let i = 0; i < contents.length; i++) {
   headers[i].addEventListener('click', function() {
     let contentHeight = contents[i].scrollHeight;
     contents[i].classList.toggle('accordion__content_visible');
+    icons[i].classList.toggle('fa-rotate-90');
     if (contents[i].classList.contains('accordion__content_visible')) {
       contents[i].style.height = contentHeight + 'px';
     } else {
