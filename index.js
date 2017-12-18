fab99ae72d92dbf9011104a1926b4bf760489e9c
Grigory Moshkevich <ilcostuff@gmail.com>
2017-12-18 23:37:53 +0300

    вроде готово, осталось отсутпы, размеры

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var headers = document.querySelectorAll('.accordion__header');
 var contents = document.querySelectorAll('.accordion__content');
 var chevrons = document.querySelectorAll('.accordion__chevron');
 
+var menuItems = document.querySelectorAll('.menu__item');
+var states = [1];
+
 for (let i = 0; i < contents.length; i++) {
   headers[i].addEventListener('click', function() {
     let contentHeight = contents[i].scrollHeight;
@@ -12,5 +15,17 @@ for (let i = 0; i < contents.length; i++) {
     } else {
       contents[i].style.height = null;
     }
+  });
+}
+
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('click', function() {
+    states.push(parseInt(menuItems[i].dataset.id));
+    console.log(states);
+    if (states[0] !== states[1]) {
+      menuItems[states[1]-1].classList.add('menu__item_active');
+      menuItems[states[0]-1].classList.remove('menu__item_active');
+    }
+    states.shift();
   });
 }
